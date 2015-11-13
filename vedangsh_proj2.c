6ce813263c2b6e38fd31e57d55c2868f68c248cd
vedangshastri <vedang.shastri@yahoo.com>
2015-11-13 00:04:05 -0500

    The Beginning

---
+++ vedangsh_proj2.c
@@ -0,0 +1,17 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdlib.h>
+
+//Header File inclusion
+//Variable declarations
+int SerSock,CliSock
+int main()
+{
+	struct sockaddr_in SerAddr;
+	int port;
+	
+	
+}
