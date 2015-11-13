67f9bfd4a1bfcb16b9ee3624d3cc840be5d4f743
vedangshastri <vedang.shastri@yahoo.com>
2015-11-13 18:44:01 -0500

    get IP using gethostbyname

--- vedangsh_proj2.c
+++ vedangsh_proj2.c
@@ -4,14 +4,44 @@
 #include <arpa/inet.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <netdb.h>
 
 //Header File inclusion
 //Variable declarations
-int SerSock,CliSock
-int main()
+int SerSock,CliSock;
+struct sockaddr_in SerAddr;
+struct sockaddr_in CliAddr;
+struct hostent *he;
+char host[100];
+int main(int argc, char argv [])
 {
-	struct sockaddr_in SerAddr;
-	int port;
 	
+	int port;
+	memset(&SerAddr,0,sizeof(SerAddr));
+	memset(&CliAddr,0,sizeof(CliAddr));
+	SerSock=socket(AF_INET, SOCK_DGRAM,0);
+	gethostname(host,100);
+	puts(host);
+	he=gethostbyname(host);
+	if(he==NULL)
+	{
+		herror("\nSomething wrong with gethostbyname");
+		exit(1);
+	}
+	printf("\nHost name: %s",he->hname);
+	printf("\nIP Address: %s",inet_ntoa(*(struct in_addr*)he->h_addr));
+
+	if(SerSock<0)
+	{
+		perror("\nSocket could not be  created");
+	}
+	SerAddr.sin_family=AF_INET;
+	port=atoi(argv[2]);
+	SerAddr.sin_port=htons(port);
+
+	SerAddr.sin_addr.s_addr=*(struct in_addr*)he->h_addr);
+	
+	return 0;
+
 	
 }
